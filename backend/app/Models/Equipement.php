c8043d64e4d04bcd3c1a1c979411cf634cbfed2b
guyjoseph <guyjosephboathiemele@gmail.com>
2026-02-22 15:48:05 +0000

    creation model equipement

---
+++ backend/app/Models/Equipement.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Equipement extends Model
+{
+    use HasFactory;
+
+    protected $table = 'equipements';
+
+    protected $fillable = [
+        'nom',
+    ];
+
+    
+}
