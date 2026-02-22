ad913637cba46ef041ce3fb3aaf22be58e04d8fd
guyjoseph <guyjosephboathiemele@gmail.com>
2026-02-22 23:34:03 +0000

    creation migration espace

---
+++ backend/database/migrations/2026_02_22_232558_create_espaces_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('espaces', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->float('surface');
+            $table->enum('type', ['bureau', 'salle de réunion', 'conférence']);
+            $table->decimal('tarif_journalier', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('espaces');
+    }
+};
