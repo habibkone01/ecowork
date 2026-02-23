9109dd19312c04afa1f9df5376fc2f83f462689f
guyjoseph <guyjosephboathiemele@gmail.com>
2026-02-23 17:02:08 +0000

    modification databaseSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Espace;
+use App\Models\Reservation;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,5 +19,9 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             EquipementSeeder::class,
         ]);
+
+        User::factory(10)->create();
+        Espace::factory(5)->create();
+        Reservation::factory(20)->create();
     }
 }
