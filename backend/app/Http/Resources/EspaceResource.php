38bd218fbc2c5f8e87a34dce3ba1085f11b6d394
guyjoseph <guyjosephboathiemele@gmail.com>
2026-02-24 13:50:35 +0000

    creation resource espace

---
+++ backend/app/Http/Resources/EspaceResource.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Resources;
+
+use App\Models\Equipement;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EspaceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nom' => $this->nom,
+            'surface' => $this->surface,
+            'type' => $this->type,
+            'tarif_journalier' => $this->tarif_journalier,
+            'equipements' => EquipementResource::collection($this->whenLoaded('equipements')),
+            'images' => ImageResource::collection($this->whenLoaded('images')),
+            'created_at' => $this->created_at->format('d-m-Y H:i'),
+            'updated_at' => $this->updated_at->format('d-m-Y H:i'),
+        ];
+    }
+}
