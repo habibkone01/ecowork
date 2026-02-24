229b76a89f2c9c3bab5cbf9f507271d8197ad3d0
guyjoseph <guyjosephboathiemele@gmail.com>
2026-02-24 13:40:19 +0000

    creation resource equipement et image

---
+++ backend/app/Http/Resources/ImageResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ImageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'url' => asset('storage/' . $this->url),
+            'espace_id' => $this->espace_id,
+        ];
+    }
+}
